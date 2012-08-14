70674ea76c7f36a67532e08d7be06c4328d6438e
scasagrande <scasagrande@galvant.ca>
2012-08-13 22:43:54 -0400

    Added some test code for Google account interaction

--- index.py
+++ index.py
@@ -1,9 +1,19 @@
 import webapp2
 
+from google.appengine.api import users
+
 class MainPage(webapp2.RequestHandler):
-  def get(self):
-      self.response.headers['Content-Type'] = 'text/plain'
-      self.response.out.write('Hello, webapp World!')
+	def get(self):
+		
+		user = users.get_current_user()
+		
+		self.response.headers['Content-Type'] = 'text/plain'
+		self.response.out.write('Hello World, this is Thingpool!\n')
+		
+		if user:
+			self.response.out.write( 'Your name is ' + user.nickname() )
+		else:
+			self.redirect( users.create_login_url(self.request.uri) )
 
 app = webapp2.WSGIApplication([('/', MainPage)],
                               debug=True)
