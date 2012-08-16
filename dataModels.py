57dea9dfe7b33a34ff06bcfe985bcccbc0ab8de8
Christopher Granade <cgranade@cgranade.com>
2012-08-15 20:01:26 -0400

    Added API methods to data models.

--- dataModels.py
+++ dataModels.py
@@ -22,6 +22,11 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##
 
+## TODO ########################################################################
+
+# Change all instances of "id" in __api__ methods to "uri". This has not been
+# done yet, as it depends on a final decision about API paths.
+
 ## IMPORTS #####################################################################
 
 ## GAE API ##
@@ -41,6 +46,10 @@ class Person(db.Model):
     permissions = db.IntegerProperty(required=True) # Five levels of permissions: Admin, manager, user, requested, banned
     
     @property
+    def uri(self):
+        raise NotImplementedError()
+        
+    @property
     def can_query_user(self):
         # TODO: check that this is how you query a property of Person, or if
         #       we need to run a query here.
@@ -48,21 +57,65 @@ class Person(db.Model):
         #       Consider adding that check as a decorator.
         return self.permissions >= s.USER_STATUS_USER
         
-    def __json__(self):
-        return json.dumps({
+    def __api__(self):
+        return {
             'id': self.key().id(),
             'nickname': self.user_account.nickname(),
             'g_account': self.user_account.email(),
             'role': s.USER_STATUS_DESCRIPTIONS[self.permissions]
-        })
+        }
+        
+    def __json__(self):
+        # TODO: move this into the as_json function in api.py.
+        return json.dumps(self.__api__())
+	
+class Category(db.Model):
+    name = db.StringProperty(required=True)
+    category_parent = db.SelfReferenceProperty()
+        # We don't use the GAE built-in parent/child relation support here,
+        # as that association is permenant, and we want user reconfigurable
+        # categories.
+        
+    @property
+    def uri(self):
+        raise NotImplementedError()
+        
+    def __api__(self):
+        return {
+            'id': self.key().id(),
+            'name': self.name,
+            'parent': self.category_parent.key().id() # FIXME: make this a URI.
+        }
 	
 class Item(polymodel.PolyModel):
     name = db.StringProperty(required=True) # Primary name of item (book title, product number, etc)
     name2 = db.StringProperty() # Secondary name to search on (eg author name, type of equipment)
     content = db.StringProperty(multiline=True) # Storage of supplimentary details (eg brief description/specs)
-    category = db.StringProperty() # Which type/category item is (book, electronics, tool, etc)
+    category = db.ReferenceProperty(Category, required=True) # Which type/category item is (book, electronics, tool, etc)
     
     store_location = db.StringProperty() # Where should the Thing be stored?
+    
+    @property
+    def uri(self):
+        raise NotImplementedError()
+        
+    def __api__(self):
+        data = {
+            'id': self.key().id(),
+            'name': self.name,
+            'category': self.category.uri
+        }
+        
+        if self.name2 is not None:
+            data['subname'] = self.name2
+            
+        if self.content is not None:
+            data['content'] = self.content
+            
+        if self.store_location is not None:
+            data['store_location'] = self.store_location
+            
+        return data
     
     @property
     def is_checked_out(self):
@@ -77,15 +130,42 @@ class Item(polymodel.PolyModel):
 class BookItem(Item):
     isbn = db.StringProperty(required=True) # Must be a string to support checksum-10 digit "X".
     author = db.StringProperty(required=True)
+    
+    def __api__(self):
+        data = super(BookItem, self).__api__()
+        data['isbn'] = self.isbn
+        data['author'] = self.author
+        return data        
 
 class CheckoutTransaction(db.Model):
     item = db.ReferenceProperty(Item, required=True)
     holder = db.ReferenceProperty(Person, required=True) # Who currently has this item checked out
     checkout_date = db.DateProperty(auto_now_add=True) # Record checkout date
     checkin_date = db.DateProperty(auto_now_add=False) 
+    
+    def __api__(self):
+        data = {
+            'item': self.item.uri,
+            'holder': self.holder.uri,
+            'checkout_date': self.checkout_date
+        }
+        if self.checkin_date is not None:
+            data['checkin_date'] = self.checkin_date
+        return data
 	
 class RequestTransaction(db.Model):
     item = db.ReferenceProperty(Item, required=True)
     requestor = db.ReferenceProperty(Person, required=True) # If requested, by who
     request_date = db.DateProperty(auto_now_add=True) # Record request date
     resolved_date = db.DateProperty(auto_now_add=False)
+    
+    
+    def __api__(self):
+        data = {
+            'item': self.item.uri,
+            'requestor': self.requestor.uri,
+            'request_date': self.request_date
+        }
+        if self.resolved_date is not None:
+            data['resolved_date'] = self.resolved_date
+        return data
