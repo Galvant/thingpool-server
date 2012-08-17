852eb1bbe22a64aaf517c760168ec8b8c3903881
scasagrande <scasagrande@galvant.ca>
2012-08-16 23:34:03 -0400

    Added (c) to index.py

--- index.py
+++ index.py
@@ -1,12 +1,44 @@
-import config
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##
+# index.py:
+##
+# © 2012 Steven Casagrande (scasagrande@galvant.ca) and
+#     Christopher E. Granade (cgranade@cgranade.com).
+# This file is a part of the ThingPool Server project.
+# Licensed under the AGPL version 3.
+##
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+##
 
-import webapp2
-import jinja2
-import os
+## IMPORTS #####################################################################
 
+## GAE API ##
 from google.appengine.api import users
 
+## WEB FRAMEWORK
+import webapp2
+import jinja2
+
+## PYTHON STANDARD LIBRARY ##
+import os
+
+## THINGPOOL MODULES ##
 import api
+import config
+
+## CORE SERVER #################################################################
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
